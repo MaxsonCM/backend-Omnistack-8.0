d4b491e860625b6528ce4b15dbe81930a086b10f
Maxson Carlos Maidlinger <max_maidlinger@hotmail.com>
2019-11-15 15:49:01 -0300

    Implementação do socket.io

--- src/server.js
+++ src/server.js
@@ -4,17 +4,35 @@ const mongoose = require('mongoose')
 const routes = require('./routes')
 const cors = require('cors')
 
+
 dotenv.config ()
 
-const server = express()
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+const connectedUsers = {}
+
+io.on('connection',socket => {
+    const { user } = socket.handshake.query
+
+    connectedUsers[user] = socket.id
+})
 
 mongoose.connect('mongodb+srv://' + process.env.USER +':' + process.env.PASSWORD + '@' + process.env.HOST + '?retryWrites=true&w=majority',{
     useNewUrlParser: true,
-    useUnifiedTopology: true 
+    useUnifiedTopology: true,
 })
 
-server.use(cors())
-server.use(express.json())
-server.use(routes)
+app.use((req, res, next) => {
+    req.io = io
+    req.connectedUsers = connectedUsers
+
+    return next()
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
 server.listen(process.env.PORT)
